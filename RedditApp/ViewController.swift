621c33ad2b2ec0d6a19cf06ce84f15a67b8fa0b2
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-02-18 12:37:22 +0200

    fix problem with controller

--- RedditApp/ViewController.swift
+++ RedditApp/ViewController.swift
@@ -10,9 +10,11 @@ import SDWebImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var test: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        test.text="Hello"
                 }
     }
 
