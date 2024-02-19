e2fc2e7960cab966479f355cddafe6524e4c28d1
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-02-19 23:01:46 +0200

    add basic of table code + xbib for post view

---
+++ RedditApp/PostCell.swift
@@ -0,0 +1,8 @@
+//
+//  PostCell.swift
+//  RedditApp
+//
+//  Created by Svitlana on 19.02.2024.
+//
+
+import Foundation
---
+++ RedditApp/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  RedditApp
+//
+//  Created by Svitlana on 18.02.2024.
+//
+
+import UIKit
+
+class PostView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
