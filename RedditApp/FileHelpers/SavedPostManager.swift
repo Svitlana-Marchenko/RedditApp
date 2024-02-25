d2197a685d2d2f17e00295f5719676e9ffbf64fa
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-02-25 11:12:37 +0200

    trying to do lab

---
+++ RedditApp/FileHelpers/SavedPostManager.swift
@@ -0,0 +1,8 @@
+//
+//  SavedPostManager.swift
+//  RedditApp
+//
+//  Created by Svitlana on 24.02.2024.
+//
+
+import Foundation
---
+++ RedditApp/ShareButtonDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  ShareButtonDelegate.swift
+//  RedditApp
+//
+//  Created by Svitlana on 24.02.2024.
+//
+
+import Foundation
